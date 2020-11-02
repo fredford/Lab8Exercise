1fa29d66e05f946ad8a2538f3439d240a7797e10
fredford <fredford@ualberta.ca>
2020-11-02 15:51:33 -0700

    added test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -37,7 +37,7 @@ class CityListTest {
         City city = new City("Charlottetown", "Prince Edward Island");
         cityList.add(city);
 
-        assertEquals(1, city.compareTo(cityList.getCities().get(0)));
-        assertEquals(1, mockCity().compareTo(cityList.getCities().get(1)));
+        assertEquals(2, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(2, mockCity().compareTo(cityList.getCities().get(1)));
     }
 }
